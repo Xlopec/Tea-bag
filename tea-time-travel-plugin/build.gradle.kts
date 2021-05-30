a4b12aa2140ce230f26fbf80e2d18c43a111b08b
Max Oliynick <maksimolejn720@gmail.com>
2021-05-30 23:42:09 +0300

    Android gradle plugin beta 3

--- tea-time-travel-plugin/build.gradle.kts
+++ tea-time-travel-plugin/build.gradle.kts
@@ -74,7 +74,7 @@ val ciTests by tasks.registering(Test::class) {
     group = "verification"
     description = "Prepares and runs tests relevant for CI build"
 
-    dependsOn(tasks.test.get() ?: error("No test task found in project $name"))
+    dependsOn(tasks.test.get())
 }
 
 sourceSets {
