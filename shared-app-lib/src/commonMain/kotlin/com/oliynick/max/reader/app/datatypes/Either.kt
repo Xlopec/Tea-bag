c942bff849ed78c5d7b782c80818a2caa7286545
Max Oliynick <maksimolejn720@gmail.com>
2021-11-09 15:01:14 +0200

    Added componentN functions implementation for Either interface

--- shared-app-lib/src/commonMain/kotlin/com/oliynick/max/reader/app/datatypes/Either.kt
+++ shared-app-lib/src/commonMain/kotlin/com/oliynick/max/reader/app/datatypes/Either.kt
@@ -3,15 +3,15 @@
 package com.oliynick.max.reader.app.datatypes
 
 // todo do something with bunch of Eithers
-sealed class Either<out L, out R>
+sealed interface Either<out L, out R>
 
 data class Left<L>(
-    val l: L
-) : Either<L, Nothing>()
+    val value: L
+) : Either<L, Nothing>
 
 data class Right<R>(
-    val r: R
-) : Either<Nothing, R>()
+    val value: R
+) : Either<Nothing, R>
 
 inline fun <L, R> Either(
     ifSuccess: () -> L,
@@ -25,6 +25,28 @@ inline fun <L, R, T> Either<L, R>.fold(
     right: (R) -> T
 ): T =
     when (this) {
-        is Left -> left(l)
-        is Right -> right(r)
+        is Left -> left(value)
+        is Right -> right(value)
     }
+
+//@OptIn(ExperimentalContracts::class)
+operator fun <L> Either<L, *>.component1(): L? {
+    // contracts aren't supported for operator functions,
+    // check for more info https://youtrack.jetbrains.com/issue/KT-32313
+    /*contract {
+        returnsNotNull() implies (this is Left<*>)
+    }*/
+
+    return (this as? Left)?.value
+}
+
+//@OptIn(ExperimentalContracts::class)
+operator fun <R> Either<*, R>.component2(): R? {
+    // contracts aren't supported for operator functions,
+    // check for more info https://youtrack.jetbrains.com/issue/KT-32313
+    /*contract {
+        returnsNotNull() implies (this is Right<*>)
+    }*/
+
+    return (this as? Right)?.value
+}
