15f8b9019f0dd247fa4a5996b8d728097d19ad9a
Max Oliynick <maksimolejn720@gmail.com>
2022-04-29 16:07:20 +0300

    Revert to Kotlin 1.6.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -47,7 +47,7 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation("com.android.tools.build:gradle:7.0.0")
     implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.5.3")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.20")
