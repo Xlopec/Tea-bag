99f4b605c0b19a6c925a5010667f5bb6295770df
Max Oliynick <maksimolejn720@gmail.com>
2023-02-05 14:48:32 +0200

    Compose MPP 1.3.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
     implementation("org.jetbrains.kotlin:kotlin-serialization:1.8.0")
     implementation("com.squareup.sqldelight:gradle-plugin:1.5.5")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.3.0-rc04")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.3.0")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0")
