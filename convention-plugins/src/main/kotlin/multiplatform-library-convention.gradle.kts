f69e50a514c70e80dfa92fca17c44ad32a40bf06
Max Oliynick <maksimolejn720@gmail.com>
2026-02-17 14:47:00 +0200

    rollback to iPhone 16

--- convention-plugins/src/main/kotlin/multiplatform-library-convention.gradle.kts
+++ convention-plugins/src/main/kotlin/multiplatform-library-convention.gradle.kts
@@ -52,7 +52,7 @@ kotlinMultiplatform {
     }
 
     targets.withType(KotlinNativeTargetWithSimulatorTests::class.java) {
-        testRuns["test"].deviceId = "iPhone 17"
+        testRuns["test"].deviceId = "iPhone 16"
     }
 }
 
--- samples/shared-app-lib/build.gradle.kts
+++ samples/shared-app-lib/build.gradle.kts
@@ -83,7 +83,7 @@ kotlin {
     }
 
     targets.withType(org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithSimulatorTests::class.java) {
-        testRuns["test"].deviceId = "iPhone 17"
+        testRuns["test"].deviceId = "iPhone 16"
     }
 
     targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
