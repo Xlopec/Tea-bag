8d5eb61621c8d5cdd363b5240ab3bfb0044effc5
Max Oliynick <maksimolejn720@gmail.com>
2026-03-12 13:28:42 +0200

    minor

--- samples/app/src/default/kotlin/io/github/xlopec/reader/app/AndroidAppComponent.kt
+++ samples/app/src/default/kotlin/io/github/xlopec/reader/app/AndroidAppComponent.kt
@@ -4,10 +4,12 @@ package io.github.xlopec.reader.app
 
 import android.app.Application
 import io.github.xlopec.reader.BuildConfig
+import io.github.xlopec.reader.app.command.Command
+import io.github.xlopec.tea.core.Component
 import kotlinx.coroutines.CoroutineScope
 
 fun AppComponent(
     application: Application,
     scope: CoroutineScope,
-) = Environment(BuildConfig.DEBUG, application, scope)
+): Component<Message, AppState, Command> = Environment(BuildConfig.DEBUG, application, scope)
     .let { env -> AppComponent(env, AppInitializer(application.systemDarkModeEnabled, env)) }
