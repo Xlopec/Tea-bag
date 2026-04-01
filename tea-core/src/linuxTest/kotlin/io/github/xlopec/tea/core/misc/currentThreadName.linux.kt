4435147f1ad042f4b3c78d2a1de373a6202996f5
Max Oliynick <maksimolejn720@gmail.com>
2026-04-01 18:50:18 +0300

    use pthread_self for linux

--- tea-core/src/linuxTest/kotlin/io/github/xlopec/tea/core/misc/currentThreadName.linux.kt
+++ tea-core/src/linuxTest/kotlin/io/github/xlopec/tea/core/misc/currentThreadName.linux.kt
@@ -1,25 +1,9 @@
 package io.github.xlopec.tea.core.misc
 
-import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.allocArray
-import kotlinx.cinterop.memScoped
-import kotlinx.cinterop.toKString
-import platform.linux.PR_GET_NAME
-import platform.linux.prctl
-
-// Linux thread names are max 16 chars including null terminator
-private const val NameBufferSize = 16
+import platform.posix.pthread_self
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun currentThreadName(): String {
-    memScoped {
-        val nameBuffer = allocArray<ByteVar>(NameBufferSize)
-        val ret = prctl(PR_GET_NAME, nameBuffer)
-        if (ret == 0) {
-            return nameBuffer.toKString()
-        }
-        // Handle error, e.g., ERANGE if the buffer is too small
-        error("Error getting thread name: $ret")
-    }
+    return "Thread ${pthread_self()}"
 }
